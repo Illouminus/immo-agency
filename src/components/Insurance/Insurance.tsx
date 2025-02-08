a63b2cbfd803d657b9e77583a7b4a1074300dca3
Illouminus <professionnel.baillot@gmail.com>
2025-02-08 14:30:49 +0100

    remove insurance and modified navbar

--- src/components/Insurance/Insurance.tsx
+++ src/components/Insurance/Insurance.tsx
@@ -1,39 +1,26 @@
 import React from 'react';
 import cls from './Insurance.module.css'
 import Image from "next/image";
-import vks from '../../../public/images/Insurance/vsk.png'
 import pego from '../../../public/images/Insurance/pego.svg'
-import maks from '../../../public/images/Insurance/maks.png'
-import sber from '../../../public/images/Insurance/sber.png'
 export const Insurance = () => {
-    return (
-        <div className={cls.container} id={"insurance"}>
-            <div className={cls.insurance_container}>
-                <div className={cls.insurrance_card}>
-                    <h2>Страхование объекта </h2>
-                    <p>
-                        Страхование объекта включает в себя получение полиса от надежной страховой компании,
-                        что гарантирует компенсацию в случае возникновения ущерба. Мы помогаем собирать все
-                        необходимые документы для выплаты и устраняем ущерб, обеспечивая сохранность вашего имущества
-                    </p>
-                </div>
+	return (
+		<div className={cls.container} id={"insurance"}>
+			<div className={cls.insurance_container}>
+				<div className={cls.insurrance_card}>
+					<h2>Страхование объекта </h2>
+					<p>
+						Страхование объекта надежной страховой компанией РЕСО-Гарантия, что обеспечивает компенсацию в случае возникновения ущерба.
+						Мы помогаем собирать все необходимые документы для выплаты, устраняем ущерб, обеспечивая сохранность вашего имущества.
+					</p>
+				</div>
 
-                <div className={cls.insurance_names}>
-                    <div className={cls.insurance_item}>
-                        <Image src={vks} alt={'VKS - страховая компания'} />
-                    </div>
-                    <div className={cls.insurance_item}>
-                        <Image src={pego} alt={'СОГАЗ - страховая компания'} />
-                    </div>
-                    <div className={cls.insurance_item}>
-                        <Image src={maks} alt={'МАКС - страховая компания'} />
-                    </div>
-                    <div className={cls.insurance_item} style={{paddingTop: '20px'}}>
-                        <Image src={sber} alt={'Сбербанк - страховая компания'} />
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+				<div className={cls.insurance_names}>
+					<div className={cls.insurance_item}>
+						<Image src={pego} alt={'РЕСО - страховая компания'} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,122 +1,122 @@
 "use client"
-import React, {useEffect, useState, useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import cls from './Navbar.module.css';
 import Image from 'next/image';
 import logo from '../../../public/images/img/logo.svg';
 import mobileLogo from '../../../public/images/img/logo2.svg'
-import {GiHamburgerMenu} from "react-icons/gi";
-import {AiOutlineClose} from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 import { motion } from 'framer-motion';
 import { Link } from 'react-scroll';
 
 
 export const Navbar = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [isTop, setIsTop] = useState(true);
-    const [activeLink, setActiveLink] = useState('');
-    const slideRef = useRef<HTMLElement>(null)
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-    useEffect(() => {
-        const onScroll = () => {
-            if (slideRef.current) {
-                const scrollPos = window.scrollY || document.documentElement.scrollTop;
-                setIsTop(scrollPos < slideRef.current.offsetHeight);
-                if (scrollPos <= 0) {
-                    setActiveLink('');
-                }
-            }
-        };
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [isTop, setIsTop] = useState(true);
+	const [activeLink, setActiveLink] = useState('');
+	const slideRef = useRef<HTMLElement>(null)
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+	useEffect(() => {
+		const onScroll = () => {
+			if (slideRef.current) {
+				const scrollPos = window.scrollY || document.documentElement.scrollTop;
+				setIsTop(scrollPos < slideRef.current.offsetHeight);
+				if (scrollPos <= 0) {
+					setActiveLink('');
+				}
+			}
+		};
 
-        window.addEventListener('scroll', onScroll);
+		window.addEventListener('scroll', onScroll);
 
-        return () => {
-            window.removeEventListener('scroll', onScroll);
-        };
-    }, []);
+		return () => {
+			window.removeEventListener('scroll', onScroll);
+		};
+	}, []);
 
-    return (
+	return (
 
-        <nav className={isTop ? cls.container : cls.scrolled} ref={slideRef}>
-            <div className={cls.burger} onClick={toggleMenu}>
-                {menuOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
-            </div>
-            <div className={cls.logo_container}>
-                <Image
-                    src={logo}
-                    alt={'logo'}
-                    width={300}
-                    className={`${cls.full_logo} ${!isTop && cls.hide_on_scroll}`}
-                />
-                <Image
-                    src={mobileLogo}
-                    alt={'logo'}
-                    height={60}
-                    className={`${cls.mobile_logo} ${!isTop && cls.show_on_scroll}`}
-                />
+		<nav className={isTop ? cls.container : cls.scrolled} ref={slideRef}>
+			<div className={cls.burger} onClick={toggleMenu}>
+				{menuOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
+			</div>
+			<div className={cls.logo_container}>
+				<Image
+					src={logo}
+					alt={'logo'}
+					width={300}
+					className={`${cls.full_logo} ${!isTop && cls.hide_on_scroll}`}
+				/>
+				<Image
+					src={mobileLogo}
+					alt={'logo'}
+					height={60}
+					className={`${cls.mobile_logo} ${!isTop && cls.show_on_scroll}`}
+				/>
 
-            </div>
-            <motion.div
-                className={`${cls.links_container} ${menuOpen ? cls.active : ''}`}>
-                <motion.div
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                >
-                <Link
-                    activeClass="active"
-                    to="work"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={600}
-                    className={activeLink === 'work' ? cls.activeLink : ''}
-                    onClick={() => { setActiveLink('work'); setMenuOpen(false); }}
-                    onSetActive={() => setActiveLink('work')}
-                    style={{
-                        cursor: "pointer"
-                    }}
-                >
-                    Как мы работаем
-                </Link>
-                </motion.div>
-                <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-                <Link
-                    activeClass="active"
-                    to="insurance"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={600}
-                    className={activeLink === 'insurance' ? cls.activeLink : ''}
-                    onClick={() => { setActiveLink('insurance'); setMenuOpen(false); }}
-                    onSetActive={() => setActiveLink('insurance')}
-                    style={{
-                        cursor: "pointer"
-                    }}
-                >
-                    Страховка
-                </Link>
-                </motion.div>
-                <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-                <Link
-                    activeClass="active"
-                    to="contacts"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={600}
-                    className={activeLink === 'contacts' ? cls.activeLink : ''}
-                    onClick={() => { setActiveLink('contacts'); setMenuOpen(false); }}
-                    onSetActive={() => setActiveLink('contacts')}
-                    style={{
-                        cursor: "pointer"
-                    }}
-                >
-                    Контакты
-                </Link>
-                </motion.div>
-            </motion.div>
-        </nav>
-    );
+			</div>
+			<motion.div
+				className={`${cls.links_container} ${menuOpen ? cls.active : ''}`}>
+				<motion.div
+					whileHover={{ scale: 1.1 }}
+					whileTap={{ scale: 0.9 }}
+				>
+					<Link
+						activeClass="active"
+						to="work"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={600}
+						className={activeLink === 'work' ? cls.activeLink : ''}
+						onClick={() => { setActiveLink('work'); setMenuOpen(false); }}
+						onSetActive={() => setActiveLink('work')}
+						style={{
+							cursor: "pointer"
+						}}
+					>
+						Как мы работаем
+					</Link>
+				</motion.div>
+				<motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+					<Link
+						activeClass="active"
+						to="insurance"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={600}
+						className={activeLink === 'insurance' ? cls.activeLink : ''}
+						onClick={() => { setActiveLink('insurance'); setMenuOpen(false); }}
+						onSetActive={() => setActiveLink('insurance')}
+						style={{
+							cursor: "pointer"
+						}}
+					>
+						Страхование
+					</Link>
+				</motion.div>
+				<motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+					<Link
+						activeClass="active"
+						to="contacts"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={600}
+						className={activeLink === 'contacts' ? cls.activeLink : ''}
+						onClick={() => { setActiveLink('contacts'); setMenuOpen(false); }}
+						onSetActive={() => setActiveLink('contacts')}
+						style={{
+							cursor: "pointer"
+						}}
+					>
+						Контакты
+					</Link>
+				</motion.div>
+			</motion.div>
+		</nav>
+	);
 };
